1710ad3f1921487726757cf5dd4e680f584a7a73
ashishbindraji <167048373+ashishbindraji@users.noreply.github.com>
2024-06-20 15:56:38 +0530

    init

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\DemoController;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+//get -> view
+//post ->store
+//put ->store
+//patch ->store / update
+//delete ->delete
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function(){
+    echo 'Welcome to Ashish Bindra Ji';
+});
+
+Route::any('/test/post', function(){
+    return view('test');
+});
+
+Route::get('demo', function(){
+    $name = "Ashish Bindra Ji";
+    $welcome = 'Welcome :';
+    return view('templates.demo',compact('name','welcome'));
+});
+
+Route::get('/user/create',[UserController::class,'create']);
+Route::get('/user/view',[UserController::class,'view']);
+Route::get('/invoke',[DemoController::class,'__invoke']);
+Route::resource('post', TestController::class);
