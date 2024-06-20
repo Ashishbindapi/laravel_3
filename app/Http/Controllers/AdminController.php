303b2a583c702f1f96ae5b97630f6475f11717c8
ashishbindraji <167048373+ashishbindraji@users.noreply.github.com>
2024-06-20 15:56:38 +0530

    init

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller
+{
+    public function store()
+    {
+        $data = [
+            "first_name" => "Ashish",
+            "last_name" => "Bindra",
+            "email" => "Bindra@gmail.com",
+            "mo_no" => 9044046526,
+            "gender" => "male",
+            "city_id" => 3,
+            "address" => "Mirzapur",
+            "type" => 1,
+            "is_active" => 1,
+        ];
+
+        DB::table('admins')->insert($data);
+        dd("Admin created Successfully", $data);
+    }
+    public function list()
+    {
+        $admins = DB::table('admins')->get();
+        foreach($admins as $admin){
+            dump($admin);
+        }
+        // dd($admins->toArray());
+        // dd($admins[1], $admins[0]->email);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+//get -> view
+//post ->store
+//put ->store
+//patch ->store / update
+//delete ->delete
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/login/store', [AdminController::class, 'store']);
+Route::get('/login/list', [AdminController::class, 'list']);
